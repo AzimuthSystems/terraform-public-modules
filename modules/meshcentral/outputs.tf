b87e80749535e9597c96e26c262a12d4291b7ffd
Chris Silvertooth <chris-alt001@silvertooth.us>
2025-07-03 13:13:55 -0700

    fixing directory structure

--- modules/meshcentral/outputs.tf
+++ modules/meshcentral/outputs.tf
@@ -1,34 +1,34 @@
-output "meshcentral_instance_public_ip" {
-  description = "Public IP of the MeshCentral instance"
-  value       = module.meshcentral.instance_public_ip
+output "instance_public_ip" {
+  description = "Public IP address of the MeshCentral EC2 instance"
+  value       = aws_eip.eip[0].public_ip
 }
 
-output "meshcentral_instance_public_dns" {
-  description = "Public DNS of the MeshCentral instance"
-  value       = module.meshcentral.instance_public_dns
+output "instance_id" {
+  description = "ID of the MeshCentral EC2 instance"
+  value       = aws_instance.ec2[0].id
 }
 
-output "meshcentral_instance_id" {
-  description = "Instance ID"
-  value       = module.meshcentral.instance_id
+output "instance_arn" {
+  description = "ARN of the MeshCentral EC2 instance"
+  value       = aws_instance.ec2[0].arn
 }
 
-output "meshcentral_instance_arn" {
-  description = "Instance ARN"
-  value       = module.meshcentral.instance_arn
+output "instance_public_dns" {
+  description = "Public DNS name"
+  value       = aws_instance.ec2[0].public_dns
 }
 
-output "meshcentral_instance_private_ip" {
+output "instance_private_ip" {
   description = "Private IP address"
-  value       = module.meshcentral.instance_private_ip
+  value       = aws_instance.ec2[0].private_ip
 }
 
-output "meshcentral_route53_record_fqdn" {
-  description = "FQDN of the Route53 record"
-  value       = module.meshcentral.route53_record_fqdn
+output "route53_record_fqdn" {
+  description = "The FQDN of the Route 53 record"
+  value       = aws_route53_record.dns[0].fqdn
 }
 
-output "meshcentral_config_ssm_parameter" {
-  description = "SSM parameter storing MeshCentral config"
-  value       = module.meshcentral.ssm_config_parameter_name
+output "ssm_config_parameter_name" {
+  description = "SSM parameter name storing MeshCentral config"
+  value       = aws_ssm_parameter.meshcentral_config.name
 }
